cf475cc7c61145f074623c560839da35335a5a7e
Mariia1986 <renij1986@gmail.com>
2020-11-19 14:09:14 +0200

    открытие модального окна

--- js/js-gallerry.js
+++ js/js-gallerry.js
@@ -1,15 +1,37 @@
-import gallery from "gallery-items.js";
-const galArr = gallery.map(({preview,original,description})=>
-return  `<li class="gallery__item">
+import gallery from "./gallery-items.js";
+
+const galleryRef = document.querySelector(".gallery");
+const modalRef = document.querySelector('.lightbox');
+const imgBigRef = document.querySelector('.lightbox__image');
+const btnCloseRef = document.querySelector(['data-action="close-lightbox"]')
+const galArr = gallery.map(({ preview, original, description }) => {
+  return `<li class="gallery__item">
 <a
   class="gallery__link"
-  href=${original}
+  href='${original}'
 >
   <img
     class="gallery__image"
-    src=${preview}
-    data-source=${preview}
-    alt=${description}
+    src='${preview}'
+    data-source='${original}'
+    alt='${description}'
   />
 </a>
-</li>`);
+</li>`;
+});
+
+galleryRef.insertAdjacentHTML("afterbegin", galArr.join(""));
+galleryRef.addEventListener('click', onClickGall)
+
+function onClickGall(event){
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG'){
+    return;
+  }
+ const bigImg = event.target.dataset.source
+ modalRef.classList.add('is-open')
+ imgBigRef.src=bigImg
+  btnCloseRef
+
+}
+
