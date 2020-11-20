c45508c32aeec0ed90f7b0841afe317a36e936b2
Mariia1986 <renij1986@gmail.com>
2020-11-20 17:06:19 +0200

    полпло

--- js/js-gallerry.js
+++ js/js-gallerry.js
@@ -5,6 +5,7 @@ const modalRef = document.querySelector(".lightbox");
 const imgBigRef = document.querySelector(".lightbox__image");
 const btnCloseRef = document.querySelector('[data-action="close-lightbox"]');
 const backdropRef = document.querySelector(".lightbox__overlay");
+
 const galArr = gallery.map(({ preview, original, description, i }) => {
   return `<li class="gallery__item">
 <a
@@ -32,24 +33,23 @@ function onClickGall(event) {
     return;
   }
   const bigImg = event.target.dataset.source;
-  const index = event.target.dataset.index
+  let index = event.target.dataset.index
   modalRef.classList.add("is-open");
   imgBigRef.src = bigImg;
-  imgBigRef.dataset.index = index
-  // console.log(event.target)
+ imgBigRef.dataset.index =  index
+  
 
   window.addEventListener("keydown", pressEscape);
   window.addEventListener("keydown", changeImg);
 }
 
 function changeImg(event) {
-  
-  if (event.code ==="ArrowRight"){
-    event.target.firstElementChild.dataset.index= Number(event.target.firstElementChild.dataset.index)+1
+   if (event.code ==="ArrowRight"){
+    // event.target.firstElementChild.dataset.index= Number(event.target.firstElementChild.dataset.index)+1
     // bigImg.dataset.index=index+1
-    imgBigRef.dataset.index +=1
+    imgBigRef.dataset.index=Number(imgBigRef.dataset.index)+1
     // console.log(event.target.firstElementChild.dataset.index)
-    
+    imgBigRef.src
   }}
 
 function closeModal() {
