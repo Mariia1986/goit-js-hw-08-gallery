9f3f37a64891ceaa26787cb786e25e675891421e
Mariia Randarevych <renij1986@gmail.com>
2020-11-21 17:23:28 +0300

    добавлены события на клавиши влево вправо

--- js/js-gallerry.js
+++ js/js-gallerry.js
@@ -2,7 +2,7 @@ import gallery from "./gallery-items.js";
 
 const galleryRef = document.querySelector(".gallery");
 const modalRef = document.querySelector(".lightbox");
-const imgBigRef = document.querySelector(".lightbox__image");
+const imgBigRef = document.querySelector('[data-index =""]');
 const btnCloseRef = document.querySelector('[data-action="close-lightbox"]');
 const backdropRef = document.querySelector(".lightbox__overlay");
 
@@ -33,29 +33,18 @@ function onClickGall(event) {
     return;
   }
   const bigImg = event.target.dataset.source;
-  let index = event.target.dataset.index
+  let index = event.target.dataset.index;
   modalRef.classList.add("is-open");
   imgBigRef.src = bigImg;
- imgBigRef.dataset.index =  index
-  
+  imgBigRef.dataset.index = index;
 
-  window.addEventListener("keydown", pressEscape);
-  window.addEventListener("keydown", changeImg);
+  window.addEventListener("keydown", pressKey);
 }
-
-function changeImg(event) {
-   if (event.code ==="ArrowRight"){
-    // event.target.firstElementChild.dataset.index= Number(event.target.firstElementChild.dataset.index)+1
-    // bigImg.dataset.index=index+1
-    imgBigRef.dataset.index=Number(imgBigRef.dataset.index)+1
-    // console.log(event.target.firstElementChild.dataset.index)
-    imgBigRef.src
-  }}
 
 function closeModal() {
   modalRef.classList.remove("is-open");
   imgBigRef.src = "";
-  window.removeEventListener("keydown", pressEscape);
+  window.removeEventListener("keydown", pressKey);
 }
 
 backdropRef.addEventListener("click", (event) => {
@@ -64,8 +53,26 @@ backdropRef.addEventListener("click", (event) => {
   }
 });
 
-function pressEscape(event) {
+function pressKey(event) {
   if (event.code === "Escape") {
     closeModal();
   }
+  if (event.code === "ArrowRight") {
+    getBigIMG(imgBigRef.dataset.index, +1);
+  }
+  if (event.code === "ArrowLeft") {
+    getBigIMG(imgBigRef.dataset.index, -1);
+  }
+}
+function getBigIMG(Ind, add) {
+  let activeIndex = Number(Ind) + add;
+  if (activeIndex === -1) {
+    activeIndex = gallery.length - 1;
+  }
+  if (activeIndex === gallery.length) {
+    activeIndex = 0;
+  }
+
+  imgBigRef.src = gallery[activeIndex].original;
+  imgBigRef.dataset.index = gallery[activeIndex].i;
 }
